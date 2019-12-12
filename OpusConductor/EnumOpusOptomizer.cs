841dceee5a8f8c54a55eaccb7902d3015425df0a
Rodney Giles <github@rodneygiles.net>
2019-12-12 13:37:44 -0600

    after fork

---
+++ OpusConductor/EnumOpusOptomizer.cs
@@ -0,0 +1,21 @@
+﻿namespace OpusConductor
+{
+    /// <summary>
+    /// Specifies the intended applications.
+    /// </summary>
+    public enum OpusOptimizer
+    {
+        /// <summary>
+        /// Process signal for improved speech intelligibility.
+        /// </summary>
+        Speech = 2048,
+        /// <summary>
+        /// Favor faithfulness to the original input.
+        /// </summary>
+        Clarity = 2049,
+        /// <summary>
+        /// Configure the minimum possible coding delay by disabling certain modes of operation.
+        /// </summary>
+        Speed = 2051
+    }
+}
---
+++ OpusConductor/EnumOpusSampleRate.cs
@@ -0,0 +1,30 @@
+﻿namespace OpusConductor
+{
+    /// <summary>
+    /// Specifies the samplerate qulaity.
+    /// </summary>
+    public enum OpusSampleRate
+    {
+        /// <summary>
+        /// 8k Sample Rate.
+        /// </summary>
+        8k = 8000,
+        /// <summary>
+        /// 12k Sample Rate.
+        /// </summary>
+        12k = 12000,
+        /// <summary>
+        /// 16k Sample Rate.
+        /// </summary>
+        16k = 16000,
+        /// <summary>
+        /// 24k Sample Rate.
+        /// </summary>
+        24k = 24000,
+        /// <summary>
+        /// 48k Sample Rate.
+        /// </summary>
+        48k = 48000
+    }
+}
+
